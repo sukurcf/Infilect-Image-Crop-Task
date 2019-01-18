f8626f8c0257a3379d7e0c8246e41c27a42c0311
sukurcf <sukurcf@gmail.com>
2019-01-18 17:26:57 +0530

    Getting response of cropped image as base64 encoded string. Can decode it to string if needed

--- file_upload/views.py
+++ file_upload/views.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from django.http import HttpResponse
@@ -6,6 +7,8 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 import numpy as np
+
+from imageprocessing.settings import MEDIA_ROOT
 from .serializers import FileSerializer
 
 import cv2
@@ -19,20 +22,21 @@ class FileView(APIView):
 
         if file_serializer.is_valid():
             file_serializer.save()
-            img = file_serializer['file']
-            if not str(img.value).endswith(('.jpeg', 'jpg', '.png')):
+            imgpath = file_serializer['file']
+            if not str(imgpath.value).endswith(('.jpeg', 'jpg', '.png')):
                 return Response('Please upload an image file only')
-            img = os.getcwd()+img.value
-            print(img)
+            imgpath = os.getcwd()+imgpath.value
             pts = eval(file_serializer['coordinates'].value)
-            print('pts:', type(pts))
-            cropfunction(img, pts)
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+            cropped_image_path = cropfunction(imgpath, pts)
+            with open(cropped_image_path, 'rb') as imgfile:
+                encoded_string = base64.b64encode(imgfile.read())
+                # return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+                return Response(encoded_string)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def cropfunction(img, pts):
-    img = cv2.imread(img)
+def cropfunction(imgpath, pts):
+    img = cv2.imread(imgpath)
     height = img.shape[0]
     width = img.shape[1]
     pts = eval(pts)
@@ -43,4 +47,7 @@ def cropfunction(img, pts):
     res = cv2.bitwise_and(img, img, mask=mask)
     rect = cv2.boundingRect(pts)
     cropped = res[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
-    cv2.imwrite('/home/sukumar/Downloads/cropped.png', cropped)
+    image_path = os.path.join(MEDIA_ROOT,os.path.splitext(imgpath)[0]+'_cropped.png')
+    cv2.imwrite(image_path, cropped)
+    return image_path
+
